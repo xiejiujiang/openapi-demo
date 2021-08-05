1846874bd1a95fe13e3b8fd975bbd4b1aaa50785
zhangshuanc <zhangshuanc@chanjet.com>
2021-08-05 14:45:48 +0800

    1、提交

---
+++ server/src/main/resources/runAliyun.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+CurrentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd ${CurrentDir}
+
+
+
+SETENV_DIR=${CurrentDir%/*}'/service'
+if [ -x $SETENV_DIR/setenv.sh ];then
+. $SETENV_DIR/setenv.sh
+fi
+
+
+eval exec java $JAVA_OPTS -Dfile.encoding=UTF-8 -Duser.country=US -Duser.language=en -Dspring.profiles.active=${ConfigType} -Xms1024m -Xmx4096m -Xmn256m -Xss256k -XX:PermSize=256M -XX:MaxPermSize=256M -XX:MaxTenuringThreshold=7 -XX:GCTimeRatio=19 -jar `pwd`/openapi-demo-service.jar
